cdd21e0ec5af0ffb62ef8517a4582557cd0a7b5d
kumar <kumarvemu@ggmail.com>
2022-07-18 14:09:12 +0000

    routing

--- src/App.js
+++ src/App.js
@@ -5,17 +5,27 @@ import PageHeader from './Components/Payments-UI/PageHeader';
 import FindATransaction from './Components/Payments-UI/FindATransaction';
 import NewTransaction from './Components/Payments-UI/NewTransaction';
 import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PageNotFound from './Components/Payments-UI/PageNotFound';
 
 function App() {
 
-  const [selectedPage, setSelectedPage] = useState("find");
-
   return (
+    <BrowserRouter>
     <div className="App">
-      <PageHeader setSelectedPage={setSelectedPage} />
-      {selectedPage === "find" && <FindATransaction />}
-      {selectedPage === "new" && <NewTransaction />}
-    </div>
+      
+      <PageHeader/>
+    
+        <Routes>
+        
+          <Route path="/find" element = {<FindATransaction />} />
+          <Route path="/new" element={<NewTransaction />} />
+          <Route path="/" element={<h1>Welcome to the application</h1>} />
+          <Route path="*" element={<PageNotFound />} />  
+        </Routes>
+        
+      </div>
+      </BrowserRouter>
   );
 }
 
