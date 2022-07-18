65b202487a96850bf61603e14e72a95782399590
kumar <kumarvemu@ggmail.com>
2022-07-18 09:53:15 +0000

    commit

--- src/Components/Payments-UI/TransactionRow.js
+++ src/Components/Payments-UI/TransactionRow.js
@@ -1,6 +1,7 @@
 const TransactionRow = (props) => {
     return <tr>
         <td>{props.id}</td>
+        <td>{props.orderId}</td>
         <td>{props.date}</td>
         <td>{props.country}</td>
         <td>{props.currency}</td>
